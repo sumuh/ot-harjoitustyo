f8f7b8b040e14ebd34ceba6be2a762da5f703854
Sus <sumusumu@helsinki.fi>
2019-04-12 19:32:50 +0300

    more tests

--- virtual_cat/src/test/java/domain/CatServiceTest.java
+++ virtual_cat/src/test/java/domain/CatServiceTest.java
@@ -5,6 +5,7 @@
  */
 package domain;
 
+import org.junit.After;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
@@ -24,6 +25,11 @@ public class CatServiceTest {
         this.catDao = new FakeCatDao();
         this.catService = new CatService(catDao);
         this.cat = catDao.create("testCat");
+        this.catService.save(cat);
+    }
+    
+    @After
+    public void teardown() {
         this.catDao.clearFile();
     }
     
@@ -35,14 +41,72 @@ public class CatServiceTest {
     
     @Test
     public void saveAndGetFromFileWorks() throws Exception {
-        catService.save(cat);
         Cat fileCat = catService.getCurrentCat();
         assertEquals("testCat", fileCat.getName());
     }
     
     @Test
-    public void getCurrentCatReturnsNullIfFileIsEmpty() {
+    public void getCurrentCatThrowsException() {
+        catDao.clearFile();
         assertEquals(null, catService.getCurrentCat());
     }
     
+    @Test
+    public void raiseCurrentCatStatsWork() {
+        catService.raiseCurrentCatStats();
+        assertEquals(1, catService.getCurrentCat().getHunger());
+        assertEquals(1, catService.getCurrentCat().getBoredom());
+        assertEquals(1, catService.getCurrentCat().getFatigue());
+    }
+    
+    @Test
+    public void raiseOneStatWorksWithHunger() {
+        catService.raiseOneStat("Hunger");
+        assertEquals(1, catService.getCurrentCat().getHunger());
+        assertEquals(0, catService.getCurrentCat().getBoredom());
+        assertEquals(0, catService.getCurrentCat().getFatigue());
+    }
+    
+    @Test
+    public void raiseOneStatWorksWithBoredom() {
+        catService.raiseOneStat("Boredom");
+        assertEquals(0, catService.getCurrentCat().getHunger());
+        assertEquals(1, catService.getCurrentCat().getBoredom());
+        assertEquals(0, catService.getCurrentCat().getFatigue());
+    }
+    
+    @Test
+    public void raiseOneStatWorksWithFatigue() {
+        catService.raiseOneStat("Fatigue");
+        assertEquals(0, catService.getCurrentCat().getHunger());
+        assertEquals(0, catService.getCurrentCat().getBoredom());
+        assertEquals(1, catService.getCurrentCat().getFatigue());
+    }
+    
+//    @Test(expected = Exception.class)
+//    public void raiseOneStatThrowsException() {
+//        catDao.clearFile();
+//        catService.raiseOneStat("Test");
+//    }
+    
+    @Test
+    public void feedWorks() {
+        catService.raiseOneStat("Hunger");
+        catService.feed();
+        assertEquals(0, catService.getCurrentCat().getHunger());
+    }
+    
+    @Test
+    public void playWorks() {
+        catService.raiseOneStat("Boredom");
+        catService.play();
+        assertEquals(0, catService.getCurrentCat().getBoredom());
+    }
+    
+    @Test
+    public void sleepWorks() {
+        catService.raiseOneStat("Fatigue");
+        catService.sleep();
+        assertEquals(0, catService.getCurrentCat().getFatigue());
+    }
 }
